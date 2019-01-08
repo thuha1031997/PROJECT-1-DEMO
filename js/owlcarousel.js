59f05ab17333c3c369fbaf68fae2918a3bf42f0f
NguyenVan Khoa <ngaymaisekhacoki@gmail.com>
2019-01-08 15:53:59 +0700

    lần 1

---
+++ js/owlcarousel.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+ 
+    var owl = $("#owl-demo");
+   
+    owl.owlCarousel({
+        items:3,
+        loop:true,
+        margin:5,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true
+    });
+   
+   
+  });
+  $(document).ready(function() {
+ 
+    var owl = $("#owl-demo1");
+   
+    owl.owlCarousel({
+        items:5,
+        loop:true,
+        margin:5,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true
+    });
+   
+   
+  });
+ 
